2bc4fb45f93c3cc04a2ff936c294c6c7957122d4
arthurPieniz <artur.pieniz@gmail.com>
2023-01-13 14:55:54 -0300

    'alteracao'

--- arquivo.js
+++ arquivo.js
@@ -33,11 +33,31 @@ let historias={
         historia:"Era uma vez 3 porquinhos, eles moravam com a sua mãe. Certo dia a mãe deles falou que eles estavam grandinhos já e precisavam construir sua própria casa. Então eles foram atrás dos materias para fazer a casa, o irmão caçula era o mais preguiçoso, e por isso acabu faendo uma fragil casa de palha. O irmão do meio se esforçou um pouco mais do que o mais novo e fez uma casa de madeira. o irmão mais velho, que era mais dedicado fez uma resistente casa de tijolos. Após isso um lobo faminto e malvado buscava comida, e os porquinhos foram a sua escolha começou pelo mais novo. O lobo disse abra essa porta! O porquinho recusou, mas o lobo não aceitou e soprou e soprou e a casa voou, o porquinho desesperado correu para a casa do irmão do meio. Mas lá estava o lobo novamete que soprou e soprou e sopru e soprou, a casa também não resistiu e voou, os dois irmão desesperados correram para a casa de tijolo do irmão mais velho. Porém novamente e mais famnto chegava o logo, só que, desta vez o lobo soprou e soprou, mas não conseguiu fazer a casa voar. Ele pensou e pensou e tentou invadir a casa pela chaminé, mas a chaminé estava com o fogo aceso e o lobo queimou o rabo e foi ebora e nunca mais foi visto."
     },
     hist2:{
+        nome:"A história de Godofredo",
+        descricao:"O Godo era um cueio muito pobre até que a mãe dele expulsou e um homem rico apareceu.",
+        historia:"Era uma vez um cueio chamado Godo ele era muito pobre e morava com sua mãe até que um dia passou um homem que era muito rico mas ele foi embora até que no outro dia a mãe de Godo expulsou ele de casa e o mesmo homem apareceu e levou o Godo para sua casa daí o Godo perguntou qual é seu nome daí o homem respondeu Menino Be daí o Godo morou com o Menino Be e eles ficaram felizes pra sempre."
+    },
+    hist3:{
+        nome:"Como o Boni nasceu",
+        descricao:"Boni nasceu com espírito abençoado. E hoje em dia Boni é famoso por muitos e reconhecido por muitos.",
+        historia:"Em um belo dia uma mulher grávida ganhou lindo filho que cresceu muito rápido.O nome de sua mãe era Bonona que nasceu em um minúsculo país que se chamava país dos Fofuchinhos.Quando Boni já estava grande, acabou namorando uma mulher chamada Bona.Boni encontrou uma organização com o Fofuchinho presidente do país, Eleforte,Elefote e Luca.Eles planejavam fazer um clube para participar dos esportes, principalmente o futebol e o Boni decidiu entrar para a organização...meses depois,ele conseguiram criar as equipes dos timesdos esportes e do futebol que seriam eles, ainda conseguiram expandir o país.Os times ficaram com o nome Time dos Boni.E o hoje em dia Boni é famoso por muitos respeitado por muitos."
+    },
+    hist4:{
+        nome:"Piada do monge",
+        descricao:"Um problema ocorre no carro de um homem e ele dorme em um monasterio, mas lá ouve um barulho estranho.",
+        historia:"Um certo dia um homem dirigia seu carro mas ele dá problema perto de um monastério, e pede se poderia ficar lá aquela noite. Os monges o acolhem e ele dorme lá, mas a noite escuta um barulho estranho. Pela manhã ele pergunta aos monges que som era aquele, mas eles respondem que ele não poderia saber pois não era um monge. O homem concorda e com o carro consertado pelo monge, segue sua viagem. Alguns anos depois ele acaba no mesmo problema, o carro estraga perto do mesmo monasterio. Ele novamente passa a noite lá e ouve o mesmo barulho, e pergunta novamente qual era o barulho, os monges dão a mesma reposta, que ele não poderia saber pois não era um monge. O homem então perguntou, como virar um monge, eles responderam-lhe, conte a quantidade de grão de areia e de folhas de árvore existentes no planeta. E lá foi-se o homem. 45 anos depois o homem volta e informa os números que achou. Então ele passa a ser um monge e pergunta a origem do som. Os monges o levam a uma porta de madeira, ele a abre, depois uma porta de pedra, e a abre novamente, então passa por portas de ferro, diamante, ouro, rubi, e chegando na última porta, ele finalmente a abre e descobre a origem do som, mas você não pode saber, porque não é um monge."
+    },
+    hist5:{
+        nome:"A vida de Menino Be",
+        descricao:"Menino Be tem filhos e vai morar em outro lugar",
+        historia:"Era vez um menino chamado Menino Be ele era de uma família muito rica. Daí o Menino Be foi crescendo muito até que ele namorou e teve um filho só que a namorada dele morreu  e o filho se chamava Godo daí o Godo virou técnico da Godolandia e o Menino Be virou jogador de futebol e é um dos melhores jogadores no caso ele está no top 3 melhores do mundo daí ele foi pro país dos Fofuchinhos e casou e teve uma nova namorada e teve outro filho chamado de Polvinho mas a namorada dele o traiu e ele foi morar com o Luca,Eleforte,Elefote e o Godo ficou muito feliz porque o Luca era o dindo dele daí todo mundo ficou feliz"
+    },
+    hist6:{
         nome:"",
         descricao:"",
         historia:""
     },
-    hist3:{
+    hist7:{
         nome:"",
         descricao:"",
         historia:""
