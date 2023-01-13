7ccaf92fe34399cecd513515f4692244b638191d
arthurPieniz <102185959+arthurPieniz@users.noreply.github.com>
2023-01-12 21:38:29 -0300

    upload

---
+++ arquivo.js
@@ -0,0 +1,45 @@
+let pessoas = {
+    pessoa1:{
+        nome:"Cláudio Pereira",
+        email:"claudio.pereira@gmail.com",
+        senha:"smart2020"
+    },
+    pessoa2:{
+        nome:"Butui Legal",
+        email:"butui.legal@gmail.com",
+        senha:"butui123"
+    },
+    pessoa3:{
+        nome:"Luca Naco",
+        email:"luca.naco@gmail.com",
+        senha:"timedosboni"
+    },
+    pessoa4:{
+        nome:"Bezinho Cria",
+        email:"bezinho.cria@gmail.com",
+        senha:"godofredovsk"
+    },
+    pessoa5:{
+        nome:"",
+        email:"@gmail.com",
+        senha:""
+    },
+}
+
+let historias={
+    hist1:{
+        nome:"Os três porquinhos",
+        descricao:"Os três porquinhos saem da casa de sua mãe e vão constrir cada uma a sua própria casa, porém havia um lobo faminto que morava ali por perto",
+        historia:"Era uma vez 3 porquinhos, eles moravam com a sua mãe. Certo dia a mãe deles falou que eles estavam grandinhos já e precisavam construir sua própria casa. Então eles foram atrás dos materias para fazer a casa, o irmão caçula era o mais preguiçoso, e por isso acabu faendo uma fragil casa de palha. O irmão do meio se esforçou um pouco mais do que o mais novo e fez uma casa de madeira. o irmão mais velho, que era mais dedicado fez uma resistente casa de tijolos. Após isso um lobo faminto e malvado buscava comida, e os porquinhos foram a sua escolha começou pelo mais novo. O lobo disse abra essa porta! O porquinho recusou, mas o lobo não aceitou e soprou e soprou e a casa voou, o porquinho desesperado correu para a casa do irmão do meio. Mas lá estava o lobo novamete que soprou e soprou e sopru e soprou, a casa também não resistiu e voou, os dois irmão desesperados correram para a casa de tijolo do irmão mais velho. Porém novamente e mais famnto chegava o logo, só que, desta vez o lobo soprou e soprou, mas não conseguiu fazer a casa voar. Ele pensou e pensou e tentou invadir a casa pela chaminé, mas a chaminé estava com o fogo aceso e o lobo queimou o rabo e foi ebora e nunca mais foi visto."
+    },
+    hist2:{
+        nome:"",
+        descricao:"",
+        historia:""
+    },
+    hist3:{
+        nome:"",
+        descricao:"",
+        historia:""
+    },
+}
